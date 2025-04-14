ae48e3e3648bb09f3e9f75c9c74e8f6cb624830a
virtdev <newmainmailelectronic@gmaiil.com>
2025-04-14 19:44:49 +0700

    fixed some warning

--- src/main/java/com/approf/approf/services/UserService.java
+++ src/main/java/com/approf/approf/services/UserService.java
@@ -2,7 +2,6 @@ package com.approf.approf.services;
 
 import com.approf.approf.Model.UserModel;
 import com.approf.approf.repository.UserRepository;
-import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,13 +13,12 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public boolean register(UserModel user) {
+    public void register(UserModel user) {
         if (userRepository.existsByUsername(user.getUsername())) {
-            return false;
+            return;
         }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
-        return true;
     }
 
     public UserModel findByLogin(String login) {
